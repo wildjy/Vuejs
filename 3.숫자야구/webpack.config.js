580daffb5df445fb68eb3e80f64e657116a8e284
jaeyoungYu <lucky0914@jinhak.com>
2020-03-18 18:10:14 +0900

    webpack 셋팅

--- 3.숫자야구/webpack.config.js
+++ 3.숫자야구/webpack.config.js
@@ -1,3 +1,16 @@
-{
+module.exports = {
+  //webpacking 기본 4구성, script를 통합관리 하기 위해
+  entry : {
+    app: './main.js',
+  },
+  module : { //webpack의 핵심
+    rules : [{
 
-}
+    }],
+  },
+  plugins : [],
+  output : {
+    filename : '[name].js', //app.js === [name].js
+    path : './dist', //경로
+  },
+};

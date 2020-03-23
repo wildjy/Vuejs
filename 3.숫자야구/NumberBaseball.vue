e48dda1d3d19f9dca952954b2285a82963c0e0c9
jaeyoungYu <lucky0914@jinhak.com>
2020-03-23 11:59:42 +0900

    Vue - webpack, add-del 테스트

---
+++ 3.숫자야구/NumberBaseball.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <h1>{{ result }}</h1>
+    <form action="post" v-on:submit="onSubmitForm">
+      <input type="text" maxlength="4" ref="answer" v-model="value">
+      <button type="submit">입력</button>
+    </form>
+    <div>시도 : {{ tries.length }}</div>
+    <ul>
+      <li v-for="t in tries">
+        <div>{{ t.try }}</div>
+        <div>{{ t.result }}</div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    data(){
+      return {
+        tries : [],
+        value : '',
+        result : '',
+      }
+    },
+    methods : {
+      onSubmitForm(e){
+        e.preventDefault();
+        this.tries.push({
+          try : this.value,
+          result : '홈런',
+        });
+        this.value = '';
+        this.$refs.answer.focus();
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
--- 3.숫자야구/webpack.config.js
+++ 3.숫자야구/webpack.config.js
@@ -1,16 +1,26 @@
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const path = require('path'); //node module 경로 설정
+//webpacking 기본 4구성[entry:{}, module:{}, plugins:[], output:{}], script를 통합관리 하기 위해
 module.exports = {
-  //webpacking 기본 4구성, script를 통합관리 하기 위해
-  entry : {
-    app: './main.js',
+  mode : 'development',
+  devtool : 'eval',
+  resolve : {
+    extensions : ['.js', '.vue'],
+  },
+  entry : { // 대표 js파일
+    app: path.join(__dirname, 'main.js'),
   },
   module : { //webpack의 핵심
     rules : [{
-
+      test : /\.vue$/,
+      use : 'vue-loader',
     }],
   },
-  plugins : [],
+  plugins : [
+    new VueLoaderPlugin(),
+  ],
   output : {
     filename : '[name].js', //app.js === [name].js
-    path : './dist', //경로
+    path : path.join(__dirname, 'dist'),
   },
 };
